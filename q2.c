03997d0039b5f32ab58a5c5581ecac4d8c27f081
Uday Sharma <sharmu6@mcmaster.ca>
2018-12-16 22:08:14 -0500

    I think this works?
    dont know how to check without wasting time....

--- q2.c
+++ q2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct{
   int day;
@@ -6,8 +7,17 @@ typedef struct{
   int year;
 }Date;
 
-int remove(Date* birthdates[], int* sizePtr, int index);
+int removeDate(Date* birthdates[], int* sizePtr, int index);
 
-int remove(Date* birthdates[], int* sizePtr, int index){
-
+int removeDate(Date* birthdates[], int* sizePtr, int index){
+  if(index < *sizePtr - 1){
+    free(birthdates[index]);
+    for(int i = index + 1; i < *sizePtr - 1; i++){
+      birthdates[i] = birthdates[i+1];
+    }
+    *sizePtr--;
+    return 0;
+  }
+  else
+    return 0;
 }

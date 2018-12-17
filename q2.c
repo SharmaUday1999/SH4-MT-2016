7eede49deb9cb484d11938d3a9422101fe5c1480
Uday Sharma <sharmu6@mcmaster.ca>
2018-12-16 21:44:59 -0500

    Q1 finished, starting q2, dont roast algorithms pls, they were on first attempt.

---
+++ q2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+typedef struct{
+  int day;
+  int month;
+  int year;
+}Date;
+
+int remove(Date* birthdates[], int* sizePtr, int index);
+
+int remove(Date* birthdates[], int* sizePtr, int index){
+
+}
